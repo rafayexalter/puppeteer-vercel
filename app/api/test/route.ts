ca5dcac4f4bde03d54ae6b57bf7c6f7dcd500939
Abdul Rafay <rafayexalter@gmail.com>
2023-12-12 23:56:50 +0100

    first commit

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -4,23 +4,23 @@ import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
   // Optional: If you'd like to use the legacy headless mode. "new" is the default.
-  //   chromium.setHeadlessMode = true;
+  chromium.setHeadlessMode = true;
 
-  //   // Optional: If you'd like to disable webgl, true is the default.
-  //   chromium.setGraphicsMode = false;
+  // Optional: If you'd like to disable webgl, true is the default.
+  chromium.setGraphicsMode = false;
 
-  //   const browser = await puppeteer.launch({
-  //     args: chromium.args,
-  //     defaultViewport: chromium.defaultViewport,
-  //     executablePath: await chromium.executablePath(),
-  //     headless: chromium.headless,
-  //   });
+  const browser = await puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath(),
+    headless: chromium.headless,
+  });
 
-  //   const page = await browser.newPage();
-  //   await page.goto("https://dev.to/");
-  //   const pageTitle = await page.title();
-  //   await browser.close();
+  const page = await browser.newPage();
+  await page.goto("https://dev.to/");
+  const pageTitle = await page.title();
+  await browser.close();
 
-  //return NextResponse.json({ title: pageTitle });
-  return NextResponse.json({ title: "Hello" });
+  return NextResponse.json({ title: pageTitle });
+  //return NextResponse.json({ title: "Hello" });
 }
