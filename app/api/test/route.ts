0c76cae42e589ca522c257463211018af1b455cf
Abdul Rafay <rafayexalter@gmail.com>
2023-12-13 20:34:45 +0100

    first commit

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -4,25 +4,27 @@ import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
   // Optional: If you'd like to use the legacy headless mode. "new" is the default.
-  chromium.setHeadlessMode = true;
+  //chromium.setHeadlessMode = true;
 
   // Optional: If you'd like to disable webgl, true is the default.
-  chromium.setGraphicsMode = false;
+  //chromium.setGraphicsMode = false;
 
   const browser = await puppeteer.launch({
     args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
     defaultViewport: chromium.defaultViewport,
     executablePath: await chromium.executablePath(
-      `https://github.com/Sparticuz/chromium/releases/download/v116.0.0/chromium-v116.0.0-pack.tar`
+      `https://github.com/Sparticuz/chromium/releases/download/v119.0.0/chromium-v119.0.0-pack.tar`
     ),
-    headless: chromium.headless,
+    headless: "new",
+    ignoreHTTPSErrors: true,
   });
 
   const page = await browser.newPage();
   await page.goto("https://dev.to/");
   const pageTitle = await page.title();
-  await browser.close();
 
-  return NextResponse.json({ title: pageTitle });
-  //return NextResponse.json({ title: "Hello" });
+  await browser.close();
+  return NextResponse.json({ title: pageTitle, message: "Working fine" });
+
+  //return NextResponse.json({ title: "Hello2" });
 }
